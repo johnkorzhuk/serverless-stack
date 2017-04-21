313adcbef96aef3a56d86f63c6140b88a17a8cce
John Korzhuk <jkorzhuk@gmail.com>
2017-04-20 21:01:56 -0400

    started on frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,74 @@
+import React, { Component } from 'react'
+import { Link, withRouter } from 'react-router-dom'
+import { Navbar, Nav, NavItem } from 'react-bootstrap'
+
+import './App.css'
+
+import Routes from './Routes'
+import RouteNavItem from './components/RouteNavItem/RouteNavItem'
+
+class App extends Component {
+  state = {
+    userToken: null
+  };
+
+  handleNavLink = event => {
+    event.preventDefault()
+    this.props.history.push(event.currentTarget.getAttribute('href'))
+  };
+
+  handleLogout = event => {
+    this.updateUserToken(null)
+  };
+
+  updateUserToken = userToken => {
+    this.setState({
+      userToken
+    })
+  };
+
+  render () {
+    const childProps = {
+      userToken: this.state.userToken,
+      updateUserToken: this.updateUserToken
+    }
+
+    return (
+      <div className='App container'>
+        <Navbar fluid collapseOnSelect>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <Link to='/'>Scratch</Link>
+            </Navbar.Brand>
+            <Navbar.Toggle />
+          </Navbar.Header>
+          <Navbar.Collapse>
+            <Nav pullRight>
+              {this.state.userToken
+                ? <NavItem onClick={this.handleLogout}>Logout</NavItem>
+                : [
+                  <RouteNavItem
+                    key={1}
+                    onClick={this.handleNavLink}
+                    href='/signup'
+                    >
+                      Signup
+                    </RouteNavItem>,
+                  <RouteNavItem
+                    key={2}
+                    onClick={this.handleNavLink}
+                    href='/login'
+                    >
+                      Login
+                    </RouteNavItem>
+                ]}
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+        <Routes childProps={childProps} />
+      </div>
+    )
+  }
+}
+
+export default withRouter(App)
---
+++ frontend/src/config.js
@@ -0,0 +1,6 @@
+export default {
+  cognito: {
+    USER_POOL_ID: 'us-east-1_sUJdx8WJg',
+    APP_CLIENT_ID: '7hum6sc0ncl0e6ncor3kko8u93'
+  }
+}
